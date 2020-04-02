2a3602e944bc6547f245673fda5a2c36b0d2fee0
Petra Janouchova <petra.janouchova@gmail.com>
2020-04-02 17:37:38 +0200

    aaarsh, this is hopeless

--- images/makeFold.sh
+++ images/makeFold.sh
@@ -1,11 +1,10 @@
 
-#for prefix in $(ls -1 | cut -d'_' -f1 | sort | uniq); #cuts the first 3-4 digits on _ and saves them as variable prefix
-#do 
-#	mkdir $prefix ; #creates a directory called by variable prefix
-#	done
 
-cwd=`pwd`
 for prefix in $(ls -1 | cut -d'_' -f1 | sort)
 do	
-	find . -name "$prefix" | mkdir -p $prefix | mv "$cwd/$prefix"* "$cwd/$prefix/" ; # fixed the mv by following https://unix.stackexchange.com/questions/77007/mv-cannot-stat-no-such-file-or-directory-in-shell-script
+	mkdir -p $prefix | find . -name "${prefix%}_*.jpg" | mv . /"${prefix%}" ;
+
 done
+
+
+
